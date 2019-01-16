c8b88161d94ca4ed9fde8e644aa8f8d8d0af538a
Charlie Azmitia <ccazmitia@gmail.com>
2019-01-15 20:35:17 -0500

    working bubble sort

---
+++ bubblesort.spec.js
@@ -0,0 +1,11 @@
+describe('Bubble Sort', function(){
+    it('handles an empty array', function(){
+      expect( bubbleSort([]) ).toEqual( [] );
+    });
+  });
+
+describe('Bubble Sort', function() {
+    it('handels an array', function(){
+        expect(bubbleSort([4,7,2,1])).toEqual([1,2,4,7])
+    })
+})
